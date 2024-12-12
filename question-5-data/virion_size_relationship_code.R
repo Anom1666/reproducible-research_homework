9265af4a2dd44ad31e07b83711f407098a1c8e1a
adamg421 <adam.grassly@magd.ox.ac.uk>
2024-12-12 21:16:46 +0000

    Add files via upload

--- question-5-data/virion_size_relationship_code.R
+++ question-5-data/virion_size_relationship_code.R
@@ -1,4 +1,4 @@
-#Load the necessary packages
+#Load the packages
 library(here)
 library(dplyr)
 library(ggplot2)
@@ -14,16 +14,9 @@ data <- data %>%
 linear_model = lm(log_volume ~ log_genome_length, data = data)
 summary(linear_model)
 
-
-#Load the necessary packages
-library(here)
-library(dplyr)
-library(ggplot2)
-
-#Load the data
-data = read.csv(here("question-5-data", "Cui_etal2014.csv"))
-
-#Perform the log-transformation on both virion volume and genome length
-data <- data %>%
-  mutate(log_volume = log(Virion.volume..nm.nm.nm.),
-         log_genome_length = log(Genome.length..kb.))
+#Create the plot
+ggplot(data, aes(x = log_genome_length, y = log_volume)) +
+  geom_point(color = "black", size = 3) +
+  geom_smooth(method = "lm", se = TRUE, color = "blue") +
+  labs(x = "log [Genome length (kb)]", y = "log [Virion volume (nm^3)]") +
+  theme_minimal() 
